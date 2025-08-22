25d5673be99b669d69528f502cb5749f11bebf9a
AMAN-349 <smantyagi349@gmail.com>
2025-08-22 13:26:26 +0530

    Testing

--- src/main/java/net/engineeringdigest/journalApp/service/WeatherService.java
+++ src/main/java/net/engineeringdigest/journalApp/service/WeatherService.java
@@ -11,8 +11,8 @@ import org.springframework.web.client.RestTemplate;
 
 @Service
 public class WeatherService {
-    //@Value("${weather.api.key}")
-    private static final String apikey = "6a16e570c3a551801abb07e87301e8d7";
+    @Value("${weather_api_key}")
+    private String apikey;
 
     private static final String API="http://api.weatherstack.com/current?access_key=API_KEY&query=CITY";
 
