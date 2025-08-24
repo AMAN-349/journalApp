4b8a7aa0c01931c43c5eaf4a3dd75486440d4772
AMAN-349 <smantyagi349@gmail.com>
2025-08-24 21:49:23 +0530

    pipeline fix

--- src/main/java/net/engineeringdigest/journalApp/service/EmailService.java
+++ src/main/java/net/engineeringdigest/journalApp/service/EmailService.java
@@ -1,22 +1,22 @@
-//package net.engineeringdigest.journalApp.service;
-//
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.mail.SimpleMailMessage;
-//import org.springframework.mail.javamail.JavaMailSender;
-//import org.springframework.stereotype.Service;
-//
+package net.engineeringdigest.journalApp.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
 //@Service
 //@Slf4j
-//public class EmailService {
-//    //java provides interface it implemented automatically we just give some configuration in app.yml
-//
+public class EmailService {
+    //java provides interface it implemented automatically we just give some configuration in app.yml
+
 //    @Autowired
 //    private JavaMailSender javaMailSender;
-//    //it is null if no conf. in app.yml after it bean is injected here by spring
-//
-//    public void sendEmail(String to, String subject, String body)
-//    {
+    //it is null if no conf. in app.yml after it bean is injected here by spring
+
+    public void sendEmail(String to, String subject, String body)
+    {
 //        try{
 //            SimpleMailMessage mail = new SimpleMailMessage();
 //            mail.setTo(to);
@@ -26,5 +26,5 @@
 //        } catch (Exception e) {
 //            log.error("Exception while send email ",e);
 //        }
-//    }
-//}
+    }
+}
