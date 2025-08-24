6426ed1ec00f7bc5f067482735e415037055da51
AMAN-349 <smantyagi349@gmail.com>
2025-08-24 11:07:13 +0530

    Unblock Pipeline

--- src/test/java/net/engineeringdigest/journalApp/repository/UserRepositoryImplTests.java
+++ src/test/java/net/engineeringdigest/journalApp/repository/UserRepositoryImplTests.java
@@ -5,8 +5,12 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+        "mongodb+srv://amantyagi3491:ifUwKFeyQm4t6OVg@cluster0.yf0hkgp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+})
 public class UserRepositoryImplTests {
 
     @Autowired
