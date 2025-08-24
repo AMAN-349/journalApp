c2febdc6b6ca59f769b7223597f1b6576150fdbf
AMAN-349 <smantyagi349@gmail.com>
2025-08-24 21:54:52 +0530

    pipeline fix

--- src/main/java/net/engineeringdigest/journalApp/service/EmailService.java
+++ src/main/java/net/engineeringdigest/journalApp/service/EmailService.java
@@ -6,25 +6,25 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
-//@Service
-//@Slf4j
+@Service
+@Slf4j
 public class EmailService {
     //java provides interface it implemented automatically we just give some configuration in app.yml
 
-//    @Autowired
-//    private JavaMailSender javaMailSender;
+    @Autowired
+    private JavaMailSender javaMailSender;
     //it is null if no conf. in app.yml after it bean is injected here by spring
 
     public void sendEmail(String to, String subject, String body)
     {
-//        try{
-//            SimpleMailMessage mail = new SimpleMailMessage();
-//            mail.setTo(to);
-//            mail.setSubject(subject);
-//            mail.setText(body);
-//            javaMailSender.send(mail);
-//        } catch (Exception e) {
-//            log.error("Exception while send email ",e);
-//        }
+        try{
+            SimpleMailMessage mail = new SimpleMailMessage();
+            mail.setTo(to);
+            mail.setSubject(subject);
+            mail.setText(body);
+            javaMailSender.send(mail);
+        } catch (Exception e) {
+            log.error("Exception while send email ",e);
+        }
     }
 }
