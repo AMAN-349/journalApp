a67d459bec2e562837cd46b42d89053d910a16a4
AMAN-349 <smantyagi349@gmail.com>
2025-08-24 21:59:36 +0530

    pipeline fix

--- src/test/java/net/engineeringdigest/journalApp/JournalAppApplicationTests.java
+++ src/test/java/net/engineeringdigest/journalApp/JournalAppApplicationTests.java
@@ -3,11 +3,16 @@ package net.engineeringdigest.journalApp;
 import net.engineeringdigest.journalApp.cache.AppCache;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.mail.javamail.JavaMailSender;
 
 @SpringBootTest
 class JournalAppApplicationTests {
+
+    @MockBean
+    private JavaMailSender javaMailSender;
 
 	@Test
 	void contextLoads() {
