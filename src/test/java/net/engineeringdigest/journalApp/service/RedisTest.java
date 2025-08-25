d528710ab5bea79216c0d0fc7675a54a61c7107f
AMAN-349 <smantyagi349@gmail.com>
2025-08-25 13:18:11 +0530

    Pipeline fix

--- src/test/java/net/engineeringdigest/journalApp/service/RedisTest.java
+++ src/test/java/net/engineeringdigest/journalApp/service/RedisTest.java
@@ -4,13 +4,18 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.mail.javamail.JavaMailSender;
 
 @SpringBootTest
 public class RedisTest {
 
     @Autowired
     private RedisTemplate redisTemplate;
+
+    @MockBean
+    private JavaMailSender javaMailSender;
 
     @Disabled
     @Test
